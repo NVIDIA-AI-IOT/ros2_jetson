9be530863f73c3c5cb52faa2f36139ad9f945ce7
Amey Kulkarni <ameykulkarni@nvidia.com>
2021-02-24 12:24:17 -0800

    renaming files

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -7,17 +7,27 @@ if [ "$#" -ne 2 ]; then
     exit
 fi
 
-ROS2_DISTRO=$1
+ROS_DISTRO=$1
 BASE_IMAGE=$2
 
-JP_release=l4t-r32.5
-DS_release=deepstream-5.0.1
+JP_release=r32.5
+DS_release=5.0.1
 echo $BASE_IMAGE
 
 if [ "$BASE_IMAGE" = "deepstream" ]; then
-	sudo docker build -t ros2_$ROS2_DISTRO:$DS_release -f DockerFile.$BASE_IMAGE.ROS2.$ROS2_DISTRO .
+	if [ "$ROS_DISTRO" = "foxy" ] || [ "$ROS_DISTRO" = "eloquent" ]; then
+    ROS_VERSION=ros2
+  else
+    ROS_VERSION=ros
+  fi
+	sudo docker build -t deepstream-$ROS_VERSION-$ROS_DISTRO:$DS_release -f DockerFile.$BASE_IMAGE.ROS.$ROS_DISTRO .
 elif [ "$BASE_IMAGE" = "l4tbase" ]; then
-	sudo docker build -t ros2_$ROS2_DISTRO:$JP_release -f DockerFile.$BASE_IMAGE.ROS2.$ROS2_DISTRO .
+  if [ "$ROS_DISTRO" = "foxy" ] || [ "$ROS_DISTRO" = "eloquent" ]; then
+    ROS_VERSION=ros2
+  else
+    ROS_VERSION=ros
+  fi
+	sudo docker build -t l4t-$ROS_VERSION-$ROS_DISTRO-pytorch:$JP_release -f DockerFile.$BASE_IMAGE.ROS.$ROS_DISTRO .
 else
 	echo "2nd Arguement needs to be deepstream or l4tbase"
 fi
